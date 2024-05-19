a0c253bcf70061467b50e76846cecbe2bea4f99b
shan856 <106764524+shan856@users.noreply.github.com>
2024-05-19 22:41:04 +0530

    removal

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,6 @@ import 'react-multi-carousel/lib/styles.css';
 import meter1 from "../assets/img/meter1.svg"
 import meter2 from "../assets/img/meter2.svg"
 import meter3 from "../assets/img/meter3.svg"
-import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () =>{
 
@@ -100,7 +99,6 @@ export const Skills = () =>{
         </Col>
         </Row>
         </Container> 
-{/*        <img src={colorSharp} className="background-image-left" alt="" /> */}
         </section>
       )
 }
